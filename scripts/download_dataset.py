005d3e1b49929b57652a5683ce6e13ad33bf66a9
risnyraranti999 <raranti.nin999@gmail.com>
2025-10-22 11:23:56 +0900

    Update download_dataset.py

--- scripts/download_dataset.py
+++ scripts/download_dataset.py
@@ -1,1 +1,8 @@
+import os
+
+def download_openneuro_dataset(dataset_id="ds003521", destination="data/raw"):
+    os.system(f"npx openneuro download --dataset={dataset_id} --snapshot=1.0.1 --destination={destination}")
+
+if __name__ == "__main__":
+    download_openneuro_dataset()
 
