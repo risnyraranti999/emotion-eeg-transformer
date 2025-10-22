c933046ccb879a76805708fe33a40c277e08e42e
risnyraranti999 <raranti.nin999@gmail.com>
2025-10-22 11:24:30 +0900

    Update evaluate.py

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,1 +1,10 @@
+import numpy as np
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+
+def evaluate(preds, labels):
+    acc = accuracy_score(labels, preds)
+    f1 = f1_score(labels, preds, average='macro')
+    cm = confusion_matrix(labels, preds)
+    return acc, f1, cm
+
 
